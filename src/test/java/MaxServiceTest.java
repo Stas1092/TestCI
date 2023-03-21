f69dc25b02eeef776d9f9af20d0de67962b35f8b
Stas1092 <ss9262694394@gmail.com>
2023-03-21 14:11:11 +0300

    max test

---
+++ src/test/java/MaxServiceTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaxServiceTest {
+
+    @Test
+    public void maxTest() {
+        MaxService service = new MaxService();
+
+        int a = 5;
+        int b = 3;
+
+        int expected = a;
+        int actual = service.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
